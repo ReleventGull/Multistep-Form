dccfcaa7f2bc8d9a5ba27103648a13d71616d606
Jaron <jaronchretien@yahoo.com>
2025-10-14 19:04:04 -0500

    feat(2nd form); Renders the second step for the form

---
+++ src/StepTwoForm.js
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import { PlanCard } from "./PlanCard"
+
+export const StepTwoForm = ({setCurrentStep, selectedPlan, setSelectedPlan, currentBilling, setCurrentBillling}) => {
+    const [cards, setCards] = useState([
+        {
+            text: "Arcade",
+            price: "$9/mo",
+            img: 'assets/images/icon-arcade.svg'
+        },
+        {
+            text: "Advanced",
+            price: "$12/mo",
+            img: 'assets/images/icon-advanced.svg'
+        },
+            {
+            text: "Pro",
+            price: "$15/mo",
+            img: 'assets/images/icon-pro.svg'
+        }
+
+    ])
+    const ballStyle = {
+        backgroundColor: 'var(--white)',
+        padding: '.6rem',
+        position: 'absolute',
+        borderRadius: "100%",
+        top: "50%",
+        left: "50%",
+        transform: `translate(${currentBilling == 'monthly' ? `-90%` : `-10%`}, -50%)`,
+    }
+    const handleSubmit = () => {
+        console.log('DO SUFF');
+    }
+    return (
+        <div className="stepFormContainer two">
+            <div className="stepFormContainerTop one">
+                <h1>Select your plan</h1>
+                <span className="secondTitle" >You have the option of monthly or yearly billing.</span>
+            </div>
+            <div className="stepFormContainerMiddle two">
+                <div className="planCardContainer">
+                    {
+                        cards.map((item, index) => 
+                        <PlanCard selectedPlan={selectedPlan} index={index} setSelectedPlan={setSelectedPlan} text={item.text} price={item.price} img={item.img}/>
+                        )
+                    }
+                </div >
+
+                <div className="monthlyYearlyContainer">
+                    <span style={{color: `${currentBilling == 'monthly' ? 'var(--blue-950)': 'var(--grey-500)'}`}} className="monthlyYearlyTitle">Monthly</span>
+                    <div onClick={() => setCurrentBillling(`${currentBilling == "monthly" ? "yearly" : "monthly"}`)} className="ballContainer">
+                        <div style={ballStyle} className="ball"/>
+                    </div>
+                    <span style={{color: `${currentBilling == 'yearly' ? 'var(--blue-950)': 'var(--grey-500)'}`}} className="monthlyYearlyTitle">Yearly</span>
+                </div>
+            </div>
+
+            <div className="stepFormContainerBottom two">
+                <span onClick={() => setCurrentStep(1)}>Go Back</span>
+                <button onClick={() => handleSubmit()} className="stepOneNextButton">Next Step</button>
+            </div>
+
+        </div>
+    )
+}
--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import { useState } from "react"
 import { StepCard } from "./StepCard"
 import { StepOneForm } from "./StepOneForm"
+import { StepTwoForm } from "./StepTwoForm"
 export const App = () => {
     const [steps, setSteps] = useState(["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"])
-    const [currentStep, setCurrentStep] = useState(1)
+    const [currentStep, setCurrentStep] = useState(2)
+
+
+    //For step 2 state (saving purposes on top level app)
+    const [currentBilling, setCurrentBillling] = useState("monthly")
+    const [selectedPlan, setSelectedPlan] = useState(null)
     return(
         <div className="formContainer">
             <div className="formContainerLeftSide">
@@ -20,8 +26,13 @@ export const App = () => {
                     {
                         currentStep == 1 ? 
                         <StepOneForm setCurrentStep={setCurrentStep}/>
-                        : null
+                        : currentStep == 2 ?
+                        <StepTwoForm setCurrentStep={setCurrentStep} selectedPlan={selectedPlan} setSelectedPlan={setSelectedPlan} setCurrentBillling={setCurrentBillling} currentBilling={currentBilling}/> :
+                        null
                     }
+                    
+                    
+                    
             </div>
         </div>
     )
